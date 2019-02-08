ad96028bc4f8ab122ead36a0dd5f1c371b64b034
ben Petersen <ben83pete@gmail.com>
2019-02-07 19:15:43 -0500

    added data

---
+++ StateSales/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StateSalesLibrary;
+
+namespace StateSales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stateSales = new StringDictionary();
+            
+            stateSales.Add("OH", 90416);
+            stateSales.Add("KY", 77408);
+            stateSales.Add("IN", 16588);
+            stateSales.Add("PA", 55043);
+            stateSales.Add("MI", 60099);
+            stateSales.Add("NY", 19830);
+            stateSales.Add("IL", 81154);
+            stateSales.Add("WI", 11679);
+            stateSales.Add("MN", 94207);
+            stateSales.Add("MS", 93521);
+
+
+
+            Console.WriteLine($"The person with Key # 42 is {stateSales.Get(42)}");
+
+        }
+       
+    }
+}
---
+++ StateSalesLibrary/SalesLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateSalesLibrary
+{
+    public class SalesLibrary
+    {
+        private Dictionary<string, int> Sales = new Dictionary <string, int>();
+        public void Add(string state, int sales)
+        {
+            Sales.Add(state, sales);           
+        }
+        public int Get(string state)
+        {
+            return Sales[state];
+        }
+
+    }
+}
